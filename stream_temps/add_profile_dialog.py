5a53a1aa29bc9bf7a830fbd8c2c06820d9c2fafc
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-08-25 14:03:49 +0200

    Added UI elements to add profile dialog.

--- stream_temps/add_profile_dialog.py
+++ stream_temps/add_profile_dialog.py
@@ -12,8 +12,25 @@ class AddProfileDialog(Tk.Toplevel):
         #self.minsize(282, 390)
         #self.resizable(False, False)
 
-        # Label
-        # Entry
-        # OK Button
-        # Cancel Button
+        name_label = Tk.Label(self, text="New Profile Name:")
+        name_label.grid(sticky=Tk.W, column=0, row=0)
+
+        self.name_variable = Tk.StringVar()
+        self.name_variable.set("New Profile")
+
+        name_entry = Tk.Entry(self, width=30, textvariable=self.name_variable)
+        name_entry.grid(sticky=Tk.W, column=0, row=1)
+
+        ok_cancel_button_frame = Tk.Frame(self)
+
+        ok_button = Tk.Button(ok_cancel_button_frame, text="OK", command=self.on_ok).pack(side=Tk.LEFT)
+        ok_button = Tk.Button(ok_cancel_button_frame, text="Cancel", command=self.on_cancel).pack(side=Tk.LEFT)
+
+        ok_cancel_button_frame.grid(column=0, row=2, sticky=Tk.E+Tk.S)
+
+    def on_ok(self):
+        None
+
+    def on_cancel(self):
+        None
 
