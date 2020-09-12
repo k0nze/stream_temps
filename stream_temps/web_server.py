8a264050e4b377dba402faae98568cccdbffd6dc
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-09-12 16:15:05 +0200

    Attempt to fix style.css caching.

--- stream_temps/web_server.py
+++ stream_temps/web_server.py
@@ -6,3 +6,12 @@ from .consts import *
 class WebServer(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=ROOT_DIR, **kwargs)
+
+    def end_headers(self):
+        self.send_custom_headers()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_custom_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
