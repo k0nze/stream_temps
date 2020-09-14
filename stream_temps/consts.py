2634d0047e786f4ace8fab1d638ef2068673ac6d
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-09-14 09:35:59 +0200

    Moved root_dir and templates into stream_temps directory.

--- stream_temps/consts.py
+++ stream_temps/consts.py
@@ -2,5 +2,5 @@ import os
 
 PORT = 8000
 
-ROOT_DIR = str(os.path.dirname(os.path.realpath(__file__))) + "/../root_dir"
-TEMPLATES_DIR = str(os.path.dirname(os.path.realpath(__file__))) + "/../templates"
+ROOT_DIR = str(os.path.dirname(os.path.realpath(__file__))) + "/root_dir"
+TEMPLATES_DIR = str(os.path.dirname(os.path.realpath(__file__))) + "/templates"
